8ef05d34b29f18a4166e55dc82bdf89b5f4e5abc
xoxlushka1337 <119114997+xoxlushka1337@users.noreply.github.com>
2023-02-27 15:39:01 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        let x = n * -1;
+        return (
+            parseInt(x.toString().split("").reverse().join("")) * Math.sign(x)
+        );
+    } else {
+        return (
+            parseInt(n.toString().split("").reverse().join("")) * Math.sign(n)
+        );
+    }
+};
